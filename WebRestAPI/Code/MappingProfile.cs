16401b03016d1d1f6bf5f546052d7ec63e517e21
brynnmal <brynnmal@udel.edu>
2025-04-14 23:47:09 -0400

    Final PS4

--- WebRestAPI/Code/MappingProfile.cs
+++ WebRestAPI/Code/MappingProfile.cs
@@ -2,12 +2,24 @@ using System;
 using AutoMapper;
 // DO NOT FORGET TO UNCOMMENT THIS LINE
 
-//using WebRest.EF.Models;
+using WebRest.EF.Models;
 
 namespace WebRestAPI.Code;
 
  public class MappingProfile : Profile {
      public MappingProfile() {
          // Add as many of these lines as you need to map your objects
+        CreateMap<Customer, Customer>();
+        CreateMap<Address, Address>();
+        CreateMap<AddressType, AddressType>();
+        CreateMap<CustomerAddress, CustomerAddress>();
+        CreateMap<Gender, Gender>();
+        CreateMap<Order, Order>();
+        CreateMap<OrdersLine, OrdersLine>(); 
+        CreateMap<OrderState, OrderState>();
+        CreateMap<OrderStatus, OrderStatus>();
+        CreateMap<Product, Product>();
+        CreateMap<ProductPrice, ProductPrice>();
+        CreateMap<ProductStatus, ProductStatus>();
      }
  }
